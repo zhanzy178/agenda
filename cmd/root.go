3cb2289f32b16bd6f1ec9c301628916a86da0ab4
zhanzongyuan <zhanzy5@mail2.sysu.edu.cn>
2018-10-29 10:35:49 +0800

    Add base command.

--- cmd/root.go
+++ cmd/root.go
@@ -34,16 +34,12 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "agenda",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Create meeting schedule for you.",
+	Long:  `Agenda is an useful CLI program for everyone to manage meeting.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
+	// If we don't definite Run(), then command will be not runnable and execute unrunnable will get errhelp.
+	Run: func(cmd *cobra.Command, args []string) {},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -87,7 +83,6 @@ func initConfig() {
 	}
 
 	viper.AutomaticEnv() // read in environment variables that match
-
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
 		fmt.Println("Using config file:", viper.ConfigFileUsed())
